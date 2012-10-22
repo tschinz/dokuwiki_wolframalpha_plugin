868545fc1fa4bc17c46dbbdd5d65351a2f6c9439
tschinz <zaswiki@gmail.com>
2012-10-22 15:02:14 +0200

    v1.0 github repo creation

---
+++ README.md
@@ -0,0 +1,67 @@
+# Dokuwiki Plugin Wolfram Alpha
+
+<table>
+  <tr>
+    <th align="left">Description</th>
+    <td>The Wolfram Alpha plugin let you add Wolfram Widget easily</td>
+  </tr>
+  <tr>
+    <th align="left">Author</th>
+    <td>Zahno Silvan</td>
+  </tr>
+  <tr>
+    <th align="left">Email</th>
+    <td>zaswiki@gmail.com</td>
+  </tr>
+  <tr>
+    <th align="left">Type</th>
+    <td>syntax</td>
+  </tr>
+  <tr>
+    <th align="left">Lastupdate</th>
+    <td>2012-10-22</td>
+  </tr>
+  <tr>
+    <th align="left">Tags</th>
+    <td>wolfram, alpha, widget, embed, calculation, mathematic</td>
+  </tr>
+</table>
+
+## Download
+* Download to Dokuwiki plugin folder
+* File     : https://github.com/tschinz/dokuwiki_wolframalpha_plugin/zipball/master
+
+## Versions
+* **2011-02-28**
+  * Initial version
+* **2012-10-22**
+  * Moved Repo to github
+
+## Syntax
+```
+{{wolfram>widget-id-value}}
+```
+
+You can get your widget-id-value go the the [Wolfram Alpha Widget](http://developer.wolframalpha.com/widgets/) page and choose or make your widget. 
+Choose the MediaWiki code which should look something like this:
+```
+{{#widget:WolframAlpha|id=17a1fd938ce990407de0d046c512db4}}
+```
+
+and copy the value behind the `id=`, in the above example it is:
+```
+17a1fd938ce990407de0d046c512db4
+```
+
+## Thanks
+  * Wolfram Alpha for this amazing computing engine
+
+## Documentation
+
+All documentation for the Wolfram Alpha Plugin is available online at:
+
+  * [Dokuwiki Plugin Page](http://dokuwiki.org/plugin:wolframalpha)
+  * [ZaWiki Plugin Page](http://zawiki.dyndns.org/~zas/zawiki/doku.php/tschinz:dw_wolfram)
+  * [Github Project Page](https://github.com/tschinz/dokuwiki_wolframalpha_plugin)
+
+(c) 2011 by Zahno Silvan <zaswiki@gmail.com>
---
+++ syntax.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * DokuWiki wolframalpha plugin
+ * Copyright (C) 2011 Zahno Silvan
+ * Usage:
+ *
+ * {{wolfram>widget_id}} 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the LGNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+ 
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_wolfram extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Zahno Silvan',
+            'email'  => 'zaswiki@gmail.com',
+            'date'   => '2011-10-22',
+            'name'   => 'Wolfram Alpha Plugin',
+            'desc'   => 'Add Wolfram Alpha Widgets to your Wikipage',
+            'url'    => 'http://zawiki.dyndns.org/doku.php/tschinz:dw_wolfram',
+        );
+    }
+
+    /**
+     * What kind of syntax are we?
+     */
+    function getType(){
+        return 'substition';
+    }
+
+    /**
+     * Where to sort in?
+     */
+    function getSort(){
+        return 299;
+    }
+
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+      $this->Lexer->addSpecialPattern('\{\{wolfram>.*?\}\}',$mode,'plugin_wolfram');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        switch ($state) {
+          case DOKU_LEXER_ENTER :
+            break;
+          case DOKU_LEXER_MATCHED :
+            break;
+          case DOKU_LEXER_UNMATCHED :
+            break;
+          case DOKU_LEXER_EXIT :
+            break;
+          case DOKU_LEXER_SPECIAL :
+            return $match;
+            break;
+        }
+        return array();
+    }
+
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+        if($mode == 'xhtml'){
+			// strip {{wolfram> from start
+			$data = substr($data,10);
+            // strip }} from end
+			$data = substr($data,0,-2);
+			$pluginid = $data;
+	
+			if (empty($pluginid))
+			{
+				$renderer->doc .= 'No plugin id value given';
+				return true;
+			}
+
+
+			$renderer->doc .= '<script type="text/javascript" id="WolframAlphaScript'.$pluginid.'" src="http://www.wolframalpha.com/widget/widget.jsp?id='.$pluginid.'"></script>';
+			
+            return true;
+        }
+        return false;
+    }
+}
